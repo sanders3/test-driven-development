3373eb4b19b414b6308ade5b0c02606b6b5d945b
Mark Sanders <mark.a.sanders@capgemini.com>
2021-06-04 13:06:55 +0100

    First round of shapes - Circle

---
+++ src/main/java/com/capgemini/adm/tdd/shapes/Circle.java
@@ -0,0 +1,25 @@
+package com.capgemini.adm.tdd.shapes;
+
+public final class Circle {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return Math.PI * 2.0 * radius;
+    }
+
+    public int getSides() {
+        return 0;
+    }
+}
---
+++ src/test/java/com/capgemini/adm/tdd/shapes/CircleTest.java
@@ -0,0 +1,27 @@
+package com.capgemini.adm.tdd.shapes;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CircleTest {
+    @Test
+    public void testAreaOfCircle() {
+        Circle circle = new Circle(5.0);
+        assertEquals("The area of a circle with radius 5 is 78.54.", 78.54, circle.getArea(), 0.001);
+    }
+
+    @Test
+    public void testCircumferenceOfCircle() {
+        Circle circle = new Circle(5.0);
+        assertEquals("The circumference of a circle with radius 5 is 31.416", 31.416, circle.getCircumference(), 0.0001);
+    }
+
+    @Test
+    public void testSidesOfCircle() {
+        Circle circle = new Circle(5.0);
+        assertThat("A circle doesn't have sides", circle.getSides(), is(0));
+    }
+}
